240954076a23fe9f653ffc6cc32900d9060711a4
vpohorilyak <volodymir.pohorilyak@ext.etecture.de>
2022-10-31 18:20:00 +0200

    pascal triangle solution

---
+++ index.js
@@ -0,0 +1,17 @@
+const makePascalTriangle = (triangleHeight) => {
+  const result = [];
+
+  if (triangleHeight < 1) return result;
+  result[0] = [1];
+
+  for (let i = 1; i < triangleHeight; i++) {
+    result[i] = [1];
+
+    for (let j = 0; j < result[i - 1].length; j++) {
+      nextValue = (result[i - 1][j] ?? 1) + (result[i - 1][j + 1] ?? 0);
+      result[i].push(nextValue);
+    }
+  }
+
+  return result;
+};
