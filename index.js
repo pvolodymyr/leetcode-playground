2eb192b488b0024cf32c78337faf396e7936c247
vpohorilyak <volodymir.pohorilyak@ext.etecture.de>
2022-10-31 22:18:13 +0200

    reverse_linked_list

---
+++ index.js
@@ -0,0 +1,31 @@
+const linkedList = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: {
+          value: 5,
+          next: null,
+        },
+      },
+    },
+  },
+};
+
+const reverseLinkedList = (head) => {
+  let current = head;
+  let previous = null;
+  let next;
+
+  while (current !== null) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+
+  return previous;
+};
