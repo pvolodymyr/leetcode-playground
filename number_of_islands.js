6909ce1d68d004b85f3aebb89dc68f976b8b0a92
vpohorilyak <volodymir.pohorilyak@ext.etecture.de>
2022-11-11 18:03:18 +0200

    number of islands

---
+++ number_of_islands.js
@@ -0,0 +1,47 @@
+const chunkOfFiveIslands = [
+  [0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 1, 1],
+  [1, 1, 0, 0, 0, 0, 0, 1],
+  [0, 0, 1, 0, 0, 0, 1, 0],
+];
+
+const setSurroundedCellsToZero = (grid, rowIndex, cellIndex) => {
+  if (
+    rowIndex < 0 ||
+    cellIndex < 0 ||
+    rowIndex >= grid.length ||
+    cellIndex >= grid[rowIndex].length ||
+    grid[rowIndex][cellIndex] === 0
+  ) {
+    return;
+  }
+
+  grid[rowIndex][cellIndex] = 0;
+
+  setSurroundedCellsToZero(grid, rowIndex - 1, cellIndex);
+  setSurroundedCellsToZero(grid, rowIndex + 1, cellIndex);
+  setSurroundedCellsToZero(grid, rowIndex, cellIndex - 1);
+  setSurroundedCellsToZero(grid, rowIndex, cellIndex + 1);
+};
+
+const calculateNumberOfIslands = (grid) => {
+  let count = 0;
+
+  for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+    for (let cellIndex = 0; cellIndex < grid[rowIndex].length; cellIndex++) {
+      if (grid[rowIndex][cellIndex] === 1) {
+        count += 1;
+
+        setSurroundedCellsToZero(grid, rowIndex, cellIndex);
+      }
+    }
+  }
+
+  return count;
+};
+
+const result = calculateNumberOfIslands(chunkOfFiveIslands);
+
+console.log(`result: founded ${result} of 5 islands`);
