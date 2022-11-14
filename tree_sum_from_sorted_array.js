6d11088569836d215c359320929452d28c7d5242
vpohorilyak <volodymir.pohorilyak@ext.etecture.de>
2022-11-14 18:27:39 +0200

    update three sum soluiton

--- tree_sum_from_sorted_array.js
+++ tree_sum_from_sorted_array.js
@@ -5,9 +5,11 @@ const getClosestTreeSum = (numbers, targetSum) => {
   let resultSum = numbers[0] + numbers[1] + numbers[numbers.length - 1];
   let addedNumbers = [numbers[0], numbers[1], numbers[numbers.length - 1]];
 
-  for (let i = 1; i < numbers.length - 2; i++) {
+  loop: for (let i = 1; i < numbers.length - 2; i++) {
     let a_pointer = i + 1;
     let b_pointer = numbers.length - 1;
+
+    if (numbers[i] === numbers[i - 1]) continue;
 
     while (a_pointer < b_pointer) {
       const currentSum = numbers[i] + numbers[a_pointer] + numbers[b_pointer];
@@ -16,6 +18,14 @@ const getClosestTreeSum = (numbers, targetSum) => {
         numbers[a_pointer],
         numbers[b_pointer],
       ];
+
+      if (currentSum === targetSum) {
+        resultSum = currentSum;
+        addedNumbers = curentAddedNumbers;
+
+        break loop;
+      }
+
       if (currentSum > targetSum) {
         b_pointer -= 1;
       } else {
